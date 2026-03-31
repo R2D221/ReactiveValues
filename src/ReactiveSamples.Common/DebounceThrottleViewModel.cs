4d9f9404c0157cac06f82e13254a0c436fece992
Arturo Torres Sánchez <r2d2.art2005@outlook.com>
2026-03-30 18:38:54 -0600

    Debounce / Throttle Blazor demo

---
+++ src/ReactiveSamples.Common/DebounceThrottleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveValues.DataTypes;
+
+namespace ReactiveSamples.Common;
+
+public sealed partial class DebounceThrottleViewModel : ReactiveObject
+{
+	public int IntervalMs
+	{
+		get => Get(() => IntervalMs, initialValue: () => 100);
+		set => Set(() => IntervalMs, value);
+	}
+
+	public TimeSpan Interval => Computed(() => Interval,
+		() => TimeSpan.FromMilliseconds(IntervalMs));
+
+	public string Input
+	{
+		get => Get(() => Input, initialValue: () => "");
+		set => Set(() => Input, value);
+	}
+
+	public string DebouncedInput => Computed(() => DebouncedInput,
+		() => Debounce(() => Input, Interval));
+
+	public string ThrottledInput => Computed(() => ThrottledInput,
+		() => Throttle(() => Input, Interval));
+}
