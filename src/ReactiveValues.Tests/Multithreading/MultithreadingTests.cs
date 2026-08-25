49f29c9f89f01ba2d4631bd42949ea16866bba27
Arturo Torres Sánchez <r2d2.art2005@outlook.com>
2026-08-24 21:35:10 -0600

    First test for volatile values

---
+++ src/ReactiveValues.Tests/Multithreading/MultithreadingTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace ReactiveValues.Tests.Multithreading;
+
+[TestClass]
+public sealed class MultithreadingTests
+{
+	[TestMethod(DisplayName = "Volatile works")]
+	public async Task Test1()
+	{
+		var sw = Stopwatch.StartNew();
+		var reactive = Reactive.Volatile(() => sw.Elapsed);
+
+		var value1 = reactive.Value;
+
+		await Task.Delay(100);
+
+		var value2 = reactive.Value;
+
+		Assert(That(value1) != value2);
+	}
+}
